90b5dfc3f18451aafdf43f08928f0a4d40664828
Vito <viktorkz91@gmail.com>
2022-08-06 17:54:50 +0300

    add new files

---
+++ Lektion 2/excelletnRessult.js
@@ -0,0 +1,17 @@
+function trainingLab(input){
+    let wInMeters = Number(input[0]);
+    let hInMeters = Number(input[1]);
+    let wInCm = wInMeters * 100;
+    let hInCm = hInMeters * 100;
+    let corridor = 100 ;
+    let desckAndDoor = 3 ;
+    let seatPerW = 70 ;
+    let seatPerH = 120 ;
+    let allSeatPerW  = (wInCm - corridor) / seatPerW -20 ;
+    let allSeatPerH = hInCm / seatPerH - 60 ;
+    let desckW = ;
+    let desckH = ;
+    
+    let teacherDesk = 60 + 120;
+     let allSeats = (allDesckW * allDeskH -3)
+}
---
+++ Lektion 2/type.js
@@ -0,0 +1,9 @@
+function lesson (input) {
+if (10 === 10){
+
+    console.log. ("yes")
+}
+
+
+}
+lesson ([25])
